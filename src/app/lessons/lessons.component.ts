48a9cf0d414760672fe4e700b3e56febb39b0c57
Camilo García <gcgarciab@unal.edu.co>
2020-05-26 08:16:48 -0500

    Performance Caching Policy - Cache First, Network Last

--- src/app/lessons/lessons.component.ts
+++ src/app/lessons/lessons.component.ts
@@ -6,9 +6,9 @@ import {SwPush} from '@angular/service-worker';
 import {catchError} from 'rxjs/operators';
 
 @Component({
-    selector: 'lessons',
-    templateUrl: './lessons.component.html',
-    styleUrls: ['./lessons.component.css']
+  selector: 'app-lessons',
+  templateUrl: './lessons.component.html',
+  styleUrls: ['./lessons.component.css']
 })
 export class LessonsComponent implements OnInit {
 
