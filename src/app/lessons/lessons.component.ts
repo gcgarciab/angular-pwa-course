c58d727772c80858a64b0bf43361f9c4d1831b26
Camilo García <gcgarciab@unal.edu.co>
2020-05-26 22:24:05 -0500

    VAPID Public Key and Private Key - Learn the differences

--- src/app/lessons/lessons.component.ts
+++ src/app/lessons/lessons.component.ts
@@ -1,9 +1,9 @@
-import {Component, OnInit} from '@angular/core';
-import {LessonsService} from '../services/lessons.service';
-import {Observable, of} from 'rxjs';
-import {Lesson} from '../model/lesson';
-import {SwPush} from '@angular/service-worker';
-import {catchError} from 'rxjs/operators';
+import { Component, OnInit } from '@angular/core';
+import { LessonsService } from '../services/lessons.service';
+import { Observable, of } from 'rxjs';
+import { Lesson } from '../model/lesson';
+import { SwPush } from '@angular/service-worker';
+import { catchError } from 'rxjs/operators';
 import { NewsletterService } from '../services/newsletter.service';
 
 @Component({
@@ -16,8 +16,11 @@ export class LessonsComponent implements OnInit {
   lessons$: Observable<Lesson[]>;
   isLoggedIn$: Observable<boolean>;
 
+  readonly VAPID_PUBLIC_KEY = 'BF9TtURzl3jgXA4TxVPLNl0BVV7j9HC5coNfbgDcsGjNY_xY2BYMcG0qLhAjCKSCn5YLmbdid20py7k_Im6U2qA';
+
   constructor(
     private lessonsService: LessonsService,
+    private swPush: SwPush,
     private newsletterService: NewsletterService
   ) { }
 
@@ -30,7 +33,13 @@ export class LessonsComponent implements OnInit {
   }
 
   subscribeToNotifications() {
-
+    this.swPush.requestSubscription({
+      serverPublicKey: this.VAPID_PUBLIC_KEY
+    }).then(sub => {
+      console.log('Notification Subscription: ', sub);
+    }).catch(err => {
+      console.error('Could not subscribe to notification', err);
+    });
   }
 
 
